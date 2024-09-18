4a9dda5f6079727a88a425b654103aacac3a4661
Basil Abushihab <basilabushihab22@gmail.com>
2024-09-18 20:59:08 +0300

    initial push

---
+++ User-Server/server.js
@@ -0,0 +1,22 @@
+//imports
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+require("dotenv").config();
+
+//server constants
+const corsConfig = { origin: "http://localhost:5173", credentials: true };
+const port = process.env.PORT || 3000;
+//Middlewares:
+//cors
+app.use(cors(corsConfig));
+//bodyParser
+app.use(bodyParser.json());
+
+//controllerRoutes:
+
+//server connection
+app.listen(port, () => {
+  console.log(`Running admin server on port http://localhost:${port}`);
+});

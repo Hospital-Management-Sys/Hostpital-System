0b91597ed714d0dec79a893b7883c04063756f4b
ForatThalji <thaljiforat@gmail>
2024-09-20 19:40:45 +0300

    Forat4

--- Hospital-Client/HospitalSystem/src/App.jsx
+++ Hospital-Client/HospitalSystem/src/App.jsx
@@ -3,31 +3,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-<<<<<<< HEAD
-import Booking from './pages/Booking'
 
 
-=======
->>>>>>> e9b6746693fe4f7b080149da570eb84bcf46ff54
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './layouts/navbar'; // Adjust path as needed
 import HomePage from './pages/home';
 import Footer from './layouts/footer';
-import FindDrs from './pages/FindDrs';
 import Doctors from './pages/Doctors';
 import Booking from './pages/Booking';
 import Appointment from './pages/Appointment';
 import AppointmentCalendar from './pages/AppointmentCalendar';
 import FindDrs from './pages/FindDrs';
-import Doctors from './pages/Doctors';
 import DoctorProfile from './components/doctorprofile'
 import AboutUsSection from './pages/about'
-<<<<<<< HEAD
-import Appointment from './pages/Appointment';
-import AppointmentCalendar from './pages/AppointmentCalendar';
-=======
->>>>>>> e9b6746693fe4f7b080149da570eb84bcf46ff54
 import LoginForm from './components/loginForm'
 
 
